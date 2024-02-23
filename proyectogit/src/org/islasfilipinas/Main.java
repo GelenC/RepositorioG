f13e9ab741d180f138129de71e7e50f56ace89c0
gelen <gelen@192.168.1.136>
2024-02-23 19:52:09 +0100

    Mi primer commit

---
+++ proyectogit/src/org/islasfilipinas/Main.java
@@ -0,0 +1,16 @@
+package org.islasfilipinas;
+/**
+ * @author gelen
+ */
+public class Main {
+	/**
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hola");
+
+	}
+
+}
